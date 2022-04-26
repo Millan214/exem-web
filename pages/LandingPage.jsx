f87d8324dee4af4672730c2bea07c66ecbf2ffda
saraalvarex <90460310+Saraalvarex@users.noreply.github.com>
2022-04-27 00:30:02 +0200

    Landing
    Cursos más vendidos

--- pages/LandingPage.jsx
+++ pages/LandingPage.jsx
@@ -24,6 +24,38 @@ const SVideoContent = styled.div`
   z-index: 2;
 `
 
+const CardContainer = styled.div`
+  box-shadow: 0 15px 30px 1px var(--color3);
+	background: rgba(255, 255, 255, 0.90);
+	text-align: center;
+	border-radius: 5px;
+	overflow: hidden;
+	margin: 5em auto;
+	height: 350px;
+	width: 700px;
+`
+
+const TopCursos = styled.div`
+	position: relative;
+	text-align: left;
+	overflow: hidden;
+	padding: 30px;
+	height: 100%;
+	float: left;
+	width: 40%;
+`
+
+const ImgCursos = styled.div`
+	transition: all 0.3s ease-out;
+	display: inline-block;
+	position: relative;
+	overflow: hidden;
+	height: 100%;
+	float: right;
+	width: 50%;
+	display: inline-block;
+`
+
 const LandingPage = () => {
 
   return (
@@ -41,6 +73,14 @@ const LandingPage = () => {
             <h1>jamon</h1>
           </FlexLayout>
         </ScreenHeightLayout>
+        <CardContainer>	
+        <TopCursos>
+            <h1>Cursos más venidos</h1>
+         </TopCursos>
+         <ImgCursos>
+            <img src="https://cdn.pixabay.com/photo/2018/11/05/09/53/massage-3795691_960_720.jpg" alt="top-cursos"/>
+          </ImgCursos>
+        </CardContainer>
       </SideMarginsLayout>
     </>
   );
